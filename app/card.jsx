c21ef895916aada1903899e86040d53703f13f1b
dzakyabdurhmn <m.dzaky.abd123@gmail.com>
2023-12-18 15:08:16 +0700

    <fixed>[paralax card and hero section}

--- app/card.jsx
+++ app/card.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import Lottie from "lottie-react";
-// import AnimationHeader from "@/assets/anim.json";
-// import { Button } from "@/components/button";
-// import Image from "next/image";
-// const App = () => {
-//   return (
-//     <>
-//       <div className="mt-44 flex flex-col text-center space-y- justify-center items-center text-[#273037]">
-//         <h1 className="text-8xl  font-semibold md:text-sm sm:text-sm">
-//           Significalty <br /> Improve self
-//         </h1>
-//         <p className="text-lg md:text-sm sm:text-sm">
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-//         </p>
-//         <div className="flex space-x-3">
-//           <Button.black /> <Button.white />
-//         </div>
-//         <Image
-//           height={10000}
-//           width={10000}
-//           className="mt-32"
-//           alt="Helloo"
-//           src={
-//             "https://res.cloudinary.com/dfnxmrqmt/image/upload/v1702508444/djub7secqkeebyqzbvvo.png"
-//           }
-//         />
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
-
 "use client";
 import styles from "./page.module.scss";
 import { projects } from "../data";
@@ -59,7 +25,7 @@ export default function Home() {
   });
 
   return (
-    <main ref={container} className={`${styles.main}`}>
+    <main ref={container} className={styles.main}>
       {projects.map((project, i) => {
         const targetScale = 1 - (projects.length - i) * 0.05;
         return (
--- app/hero.tsx
+++ app/hero.tsx
@@ -2,25 +2,25 @@ import React from "react";
 import "./style.css";
 function abisHero() {
   return (
-    <main className="mt-[74px] justify-center items-center flex flex-col space-y-12">
+    <main className="mt-[74px] justify-center items-center flex flex-col space-y-12 md:ml-16 md:mr-16">
       <div>
         <div className="text-center uppercase text-lg">
           <h1 className="text-lg text-blue-700">for humanity</h1>
-          <h1 className="mt-7 text-5xl   font-semibold leading-normal text-[#0E0B3D]">
+          <h1 className="mt-7 text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold leading-relaxed sm:leading-10 lg:leading-normal text-[#0E0B3D]">
             MEETING ALL{" "}
             <span className="bg-[#AAFF00] px-3 py-2 rounded-2xl">
               STUDENTS'
             </span>{" "}
             <br /> NEEDS IN ONE GRASP.
           </h1>
-          <p className="mt-6 text-[#0E0B3D]">
+          <p className="mt-5 md:text-base text-[#0E0B3D]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, cum!
           </p>
         </div>
       </div>
       {/*  */}
 
-      <div className="stage md:-mb-11">
+      <div className="stage">
         <img
           className="box bounce-1"
           src="https://res.cloudinary.com/dfnxmrqmt/image/upload/v1702753952/iyffpzktqq2dencmjg4w.svg"
