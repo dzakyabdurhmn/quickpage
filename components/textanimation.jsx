d61aa0a4ca682056d72a827d7f1bb47329bd10b0
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-01-04 02:45:58 +0700

    (ui):[text animation]

---
+++ components/textanimation.jsx
@@ -0,0 +1,94 @@
+"use client";
+import { useEffect } from "react";
+import styled from "styled-components";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const Title = styled.p`
+  font-size: 3rem;
+  font-weight: 600;
+`;
+
+const Word = styled(motion.span)`
+  display: inline-block;
+  margin-right: 0.25em;
+  white-space: nowrap;
+`;
+
+const Character = styled(motion.span)`
+  display: inline-block;
+  margin-right: -0.05em;
+`;
+
+export default function AnimatedTitle() {
+  const text = " MEETING ALL STUDENT NEEDS IN ONE GRASP."; // This would normally be passed into this component as a prop!
+
+  const ctrls = useAnimation();
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      ctrls.start("visible");
+    }
+    if (!inView) {
+      ctrls.start("hidden");
+    }
+  }, [ctrls, inView]);
+
+  const wordAnimation = {
+    hidden: {},
+    visible: {},
+  };
+
+  const characterAnimation = {
+    hidden: {
+      opacity: 0,
+      y: `0.25em`,
+    },
+    visible: {
+      opacity: 1,
+      y: `0em`,
+      transition: {
+        duration: 1,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
+  };
+
+  return (
+    <Title aria-label={text} role="heading">
+      {text.split(" ").map((word, index) => {
+        return (
+          <Word
+            ref={ref}
+            aria-hidden="true"
+            key={index}
+            initial="hidden"
+            animate={ctrls}
+            variants={wordAnimation}
+            transition={{
+              delayChildren: index * 0.25,
+              staggerChildren: 0.05,
+            }}
+          >
+            {word.split("").map((character, index) => {
+              return (
+                <Character
+                  aria-hidden="true"
+                  key={index}
+                  variants={characterAnimation}
+                >
+                  {character}
+                </Character>
+              );
+            })}
+          </Word>
+        );
+      })}
+    </Title>
+  );
+}
